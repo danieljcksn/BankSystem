a6dcf0c2d0dc295d8c9990c50f8ceceff09a5d78
brenu <bvsousa64@gmail.com>
2019-05-11 18:49:06 -0300

    atts bloco 1

--- src/br/clientes/lista212019/cet078/dcet/uesc/Cliente.java
+++ src/br/clientes/lista212019/cet078/dcet/uesc/Cliente.java
@@ -10,5 +10,29 @@ package br.clientes.lista212019.cet078.dcet.uesc;
  * @author brenu
  */
 public class Cliente {
+    private String nome;
+    private int cpf;
+
+    public Cliente(String nome, int cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+    
     
 }
--- src/br/contas/lista212019/cet078/dcet/uesc/Conta.java
+++ src/br/contas/lista212019/cet078/dcet/uesc/Conta.java
@@ -5,10 +5,50 @@
  */
 package br.contas.lista212019.cet078.dcet.uesc;
 
+import br.clientes.lista212019.cet078.dcet.uesc.Cliente;
 /**
  *
  * @author brenu
  */
 public class Conta {
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+    
+    public Conta(int numero, double saldo, Cliente cliente){
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+    
+    public Conta(int numero, Cliente cliente){
+        this.numero = numero;
+        this.cliente = cliente;
+    }   
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cliente getCliente() {
+        return this.cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
     
 }
